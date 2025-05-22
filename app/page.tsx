d89d04700bdcbf7a8a2baf86018b0dee0190aee3
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-05-22 09:13:34 +0600

    add hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return "Hello";
+import Hero from "@/components/Hero";
+import { hero } from "@/db/hero.json";
+
+export default async function Home() {
+  return (
+    <>
+      <Hero hero={hero} />
+    </>
+  );
 }
